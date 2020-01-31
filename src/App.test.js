a37169645714acd803f3f0cc50b9f167f360d593
chrisxkoch <chrxkoch@icloud.com>
2020-01-31 20:34:55 +0100

    tests update

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { shallow } from 'enzyme';
+import App from '../App';
+import EventList from '../EventList';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('<App /> component', () => {
+  test('render list of events', () => {
+    const AppWrapper = shallow(<App />);
+    expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
 });
